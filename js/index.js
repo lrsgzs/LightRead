8410e5e3ad735bc44913a3c230cc8d61e37ea2ad
lrs2187 <liurongshuo2022@outlook.com>
2024-11-05 22:57:18 +0800

    💫feat: update time & load article & different design

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-import { marked } from './marked.esm.js';
-import purify from './purify.es.mjs';
-import highlight from './highlight.js';
-import article from './article.js';
+import { marked } from 'https://fastly.jsdelivr.net/npm/marked/lib/marked.esm.js';
+// import purify from './purify.es.mjs';
+// import highlight from './highlight.js';
+import article from './articles.js';
 
+const container = document.getElementById('container');
+const time_container = document.getElementById('time-container');
+const articles = await article.get_articles();
+const articles_count = articles.length;
+const article_content = await article.load_article(1);  // for debugging only
+container.innerHTML = marked.parse(article_content);
+
+setInterval(() => {
+    const date = new Date();
+    time_container.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+}, 100)
